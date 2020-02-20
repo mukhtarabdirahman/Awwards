dfc1e6758df6eaa3cbf83839fea0a56bee734a82
mukhtarabdirahman <mukhtarabdirahmamn@gmail.com>
2020-02-20 16:32:03 +0300

    uploading

--- jake/models.py
+++ jake/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    profilePic = models.ImageField(upload_to='profile/',null=True)
+    bio = models.CharField(max_length=60,blank=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+    def create_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+class Image(models.Model):
+    image = models.ImageField()
+    caption = models.CharField(max_length = 60)
+    upload_date = models.DateTimeField(default=timezone.now)
+    # profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    likes = models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        return self.caption
+    class Meta:
+        ordering = ['-upload_date']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_caption(cls,id,caption):
+        captioned = Image.objects.filter(id=id).update(caption = caption)
+        return captioned
+
+    @classmethod
+    def get_all(cls):
+        imgs = Image.objects.all()
+        return imgs
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = Image.objects.filter(id=Image.id)
+        return image

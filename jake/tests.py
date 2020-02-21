1a52df2d20cc8c89ec9d18ca06730281aaa468b9
mukhtarabdirahman <mukhtarabdirahmamn@gmail.com>
2020-02-21 16:13:20 +0300

    test

--- jake/tests.py
+++ jake/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Editor,Article,tag
+import datetime as dt
 
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.ahmed= Editor(first_name = 'ahmed', last_name ='Mukhtar', email ='ahmed@moringaschool.com')
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.ahmed,Editor))
+    
+    # Testing Save Method
+    def test_save_method(self):
+        self.ahmed.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+        

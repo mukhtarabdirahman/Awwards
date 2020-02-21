f0d025ebbaaba939d7bf823bacbf434875ff5fe6
mukhtarabdirahman <mukhtarabdirahmamn@gmail.com>
2020-02-21 16:13:57 +0300

    test

--- jake/tests.py
+++ jake/tests.py
@@ -18,3 +18,17 @@ class EditorTestClass(TestCase):
         editors = Editor.objects.all()
         self.assertTrue(len(editors) > 0)
         
+class ArticleTestClass(TestCase):
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.ahmed= Editor(first_name = 'ahmed', last_name ='Mukhtar', email ='ahmed@moringaschool.com')
+        self.ahmed.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.ahmed)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
